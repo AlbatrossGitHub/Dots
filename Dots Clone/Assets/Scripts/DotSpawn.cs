fb68ad7afeb2232aa46aced108aaf9cb347130aa
Josh Finkel <jlf581@nyu.edu>
2021-04-07 11:25:40 -0400

    Dot spawning and making them fall

---
+++ Dots Clone/Assets/Scripts/DotSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotSpawn : MonoBehaviour
+{
+
+    public GameObject dot;
+
+    public int x;
+
+    public int y;
+
+    public float dist;
+
+    public int numOfDots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnDot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnDot()
+    {
+        for(int j = 0; j < numOfDots; j++){
+            for(int i = 0; i < numOfDots; i++){
+                Instantiate(dot, new Vector3(x+i*dist, y+j, 0), Quaternion.identity);
+                //Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+            }
+        }
+    }
+
+}
+
