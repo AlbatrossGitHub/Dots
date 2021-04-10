26577bfe3c32dc7b606d0fc4d3185dc1fa1dbe86
CactusLamp <shanglan.may.li@gmail.com>
2021-04-10 17:18:41 -0400

    attempt to program the line between dots

--- Dots Clone/Assets/Scripts/OtherDotBehavior.cs
+++ Dots Clone/Assets/Scripts/OtherDotBehavior.cs
@@ -12,17 +12,24 @@ public class OtherDotBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnMouseDown(){
+    void OnMouseDown() {
         myManager.selectedDot = gameObject; //lowercase G means the one we are on
         //gameObject.GetComponent<CircleCollider2D>().enabled = false;
+    }
+
+    
+
+     void OnMouseOver()
+    {
+        //if()
     }
 }
--- Dots Clone/Assets/Scripts/OtherGameManager.cs
+++ Dots Clone/Assets/Scripts/OtherGameManager.cs
@@ -6,19 +6,68 @@ public class OtherGameManager : MonoBehaviour
 {
 
     public GameObject selectedDot;
+    public Vector3 lineEnd;
+    public Vector3 dotOrigin;
+    public Vector3 mousePos;
 
-
+    //the second dot that gamemanager collider hits
+    public GameObject connectedDot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lineEnd = mousePos;
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        lineEnd = mousePos;
+        if (collision.gameObject.tag == "Dot" && collision.gameObject != selectedDot)
+        {
+            dotOrigin = collision.gameObject.transform.position;
+            lineEnd = dotOrigin;
+
+        }
+        else
+        {
+            lineEnd = mousePos;
+        }
+
+        //if(the Game manager hitbox is collding with a dot hitbox && the
+        //game manager hitbox is NOT colliding with the dot that we are getting the selectedDot variable from)
+        //{ dotOrigin = coordinates of the dot that the gamemanager is colliding with
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dot" && collision.gameObject != selectedDot)
+        {
+            lineEnd = mousePos;
+
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //get the mouse position
+        gameObject.transform.position = mousePos;
+
+        if (selectedDot != null)
+        {
+            //lineEnd = mousePos;
+
+            RaycastHit2D hit = Physics2D.Raycast(selectedDot.transform.position, lineEnd, 25f);
+            Debug.DrawLine(selectedDot.transform.position, lineEnd, Color.red);
+            if (hit != null)
+            {
+                lineEnd = dotOrigin;
+            }
+        }
+
+        /*if(Input.GetMouseButtonDown(0)){
 
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             
@@ -38,5 +87,10 @@ public class OtherGameManager : MonoBehaviour
             Debug.Log(hit);
             Debug.Log(hit.collider.gameObject.name);
         }
+        */
+
+
     }
+
+    
 }
